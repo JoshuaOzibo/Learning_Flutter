efbb644ab1d42a89a92b01c53e7878b6b951565f
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-08-16 17:24:07 +0100

    Update ProfilePage to display a list of profile items with improved layout

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
-const int itemCount = 10;
-
+const int itemCount = 20;
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: itemCount, 
-      itemBuilder: (BuildContext context, int index){
-        return ListTile();
-      });
+      itemCount: itemCount,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: Icon(Icons.person),
+          title: Text("Profile Item $index"),
+          subtitle: Text("This is item number $index"),
+          trailing: Icon(Icons.arrow_forward),
+        );
+      },
+    );
   }
 }
