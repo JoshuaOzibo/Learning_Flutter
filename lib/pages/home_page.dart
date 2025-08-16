3fcdcdc1790482eb4e189e92d7ad5a7b65b6af40
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-08-16 03:54:30 +0100

    Add LearnFlutterPage with basic UI components and functionality
    - Implemented a StatefulWidget for the LearnFlutterPage.
    - Added an AppBar with a custom back button.
    - Included an image, a container with text, and an elevated button.
    - Created a row with icons and text.
    - Added a Switch widget to toggle a boolean value.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/learn_flutter_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          
+          onPressed: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context){
+                  return const LearnFlutterPage();
+                } )
+            );
+          }, 
+          
+          child: Text('Learn Flutter')),
+      ),
+    );
   }
 }
---
+++ lib/pages/learn_flutter_page.dart
@@ -0,0 +1,85 @@
+import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
+
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    bool isboolVal = false;
+
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Learn Flutter '),
+        // automaticallyImplyLeading: false, // this is use to automatically hide automaticallyImplyLeading
+        // lets create out own leading 
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          }, 
+          icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        
+        children: [
+          Image.asset("images/learningImage.png"),
+          SizedBox(height: 20,),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            height: 200,
+            width: double.infinity,
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(10.0),
+            // decoration: ,
+            color:  Colors.blue,
+            child: const Center(
+              child: Text("Hello from my container"),
+            ),
+          ),
+
+
+          // types of button
+          ElevatedButton(
+            onPressed: (){
+              printToConsole('Hello');
+            }
+          , child: 
+          const Text('Hello elevated button', 
+          style: TextStyle(color: Colors.amber),)),
+
+          SizedBox(height: 50,),
+          Row(
+            children: [
+              Icon(Icons.fireplace),
+              Text('Hello from row', style: TextStyle(color: Colors.blue),),
+              Icon(Icons.fireplace),
+            ],
+          ),
+
+          Switch(
+            value: isboolVal, 
+            onChanged: (bool newBool) {
+              setState(() {
+                isboolVal = newBool;
+              });
+            })
+
+        ],
+      ),
+
+
+    );
+  }
+}
