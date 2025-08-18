e2a17050d5b6eee412d638bd3f0e0826a90a4513
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-08-18 14:20:48 +0100

    latest code , added select button with seperation of concerns

---
+++ lib/groceries_ui/components/select_grocery.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class SelectGrocery extends StatelessWidget {
+  const SelectGrocery({super.key, required this.filterBy});
+
+  final String filterBy;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.orange,
+      ),
+      padding: const EdgeInsets.only(left: 15, right: 15, top: 5, bottom: 5),
+      margin: const EdgeInsets.only(right: 10),
+      child: Text(filterBy, style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+    );
+  }
+}
--- lib/groceries_ui/grocery_ui.dart
+++ lib/groceries_ui/grocery_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/groceries_ui/components/select_grocery.dart';
 
 class GroceryUi extends StatelessWidget {
   const GroceryUi({super.key});
@@ -6,30 +7,143 @@ class GroceryUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 239, 239, 239),
+      // appBar: AppBar(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 239, 239, 239),
         leadingWidth: 100,
         leading: Container(
-          color: Colors.blue,
-          // padding: EdgeInsets.only(left: 10),
-          child: Align(
+          // color: Colors.blue,
+          padding: EdgeInsets.only(left: 10),
+          child: const Align(
             alignment: Alignment.centerLeft,
             child: Text('Abuja Nigeria'),
           ),
         ),
         actions: [
           Container(
-            color: Colors.blue,
+            color: Colors.white,
             margin: EdgeInsets.only(right: 10),
             padding: EdgeInsets.all(5),
-            child: Icon(Icons.search)),
+            child: Icon(Icons.search),
+          ),
           Container(
-            color: Colors.blue,
+            color: Colors.white,
             margin: EdgeInsets.only(right: 10),
             padding: EdgeInsets.all(5),
             child: Icon(Icons.shopping_cart),
           ),
         ],
+      ),
+
+      // body: Container
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black, // default color
+                ),
+                children: [
+                  TextSpan(text: 'Fresh '),
+                  TextSpan(
+                    text: 'Groceries',
+                    style: TextStyle(color: Colors.orange),
+                  ),
+                  TextSpan(text: ', Zero Hassel'),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // card
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.orange[100],
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Fruits',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green[100],
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Vegetables',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+            // SelectGrocery(),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  SelectGrocery(filterBy: 'All'),
+                  SelectGrocery(filterBy: 'Vegetables'),
+                  SelectGrocery(filterBy: 'Fruits'),
+                  SelectGrocery(filterBy: 'Meat'),
+                  SelectGrocery(filterBy: 'Bakery'),
+                  SelectGrocery(filterBy: 'Dairy'),
+                  SelectGrocery(filterBy: 'Snacks'),
+                  SelectGrocery(filterBy: 'Beverages'),
+                  SelectGrocery(filterBy: 'Frozen'),
+                  SelectGrocery(filterBy: 'Seafood'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
