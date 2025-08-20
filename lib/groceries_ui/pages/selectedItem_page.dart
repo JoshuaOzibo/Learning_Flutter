6fd78cd49dac42787cd7a8dbdf88060385d7d341
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-08-20 12:15:35 +0100

    udated selectItem page

--- lib/groceries_ui/pages/selectedItem_page.dart
+++ lib/groceries_ui/pages/selectedItem_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SelecteditemPage extends StatelessWidget {
@@ -94,7 +96,7 @@ class SelecteditemPage extends StatelessWidget {
 
                     Container(
                       decoration: BoxDecoration(
-                        color: Colors.grey,
+                        color: Color.fromARGB(255, 239, 239, 239),
                         borderRadius: BorderRadius.all(Radius.circular(15)),
                       ),
                       width: double.infinity,
@@ -104,14 +106,18 @@ class SelecteditemPage extends StatelessWidget {
                       ),
                       height: 80,
                       child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Row(children: [Icon(Icons.star), Text('4.6')]),
                               Text('Rating'),
                             ],
                           ),
                           Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Row(
                                 children: [
@@ -123,6 +129,7 @@ class SelecteditemPage extends StatelessWidget {
                             ],
                           ),
                           Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Row(children: [Icon(Icons.person), Text('3k +')]),
                               Text('Overview'),
@@ -131,10 +138,168 @@ class SelecteditemPage extends StatelessWidget {
                         ],
                       ),
                     ),
+
+                    SizedBox(height: 15),
+                    // description
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Description',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'This product is designed with high quality standards, '
+                          'offering durability, style, and comfort. Perfect for everyday use, '
+                          'it combines modern design with practical functionality to suit your needs.',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey[800],
+                            height: 1.5,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 20),
+
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: const Color.fromARGB(
+                            255,
+                            184,
+                            184,
+                            184,
+                          ), // your border color
+                          width: 0.6, // border thickness
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            spacing: 3,
+                            children: [
+                              Text(
+                                '\u20A620,000',
+                                style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 18,
+                                  color: const Color.fromARGB(
+                                    255,
+                                    162,
+                                    162,
+                                    162,
+                                  ),
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.lineThrough,
+                                  decorationColor: const Color.fromARGB(
+                                    255,
+                                    164,
+                                    164,
+                                    164,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                '\u20A65,000',
+                                style: TextStyle(
+                                  fontFamily: 'Roveto',
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Row(
+                                spacing: 8,
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(width: 1),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(100),
+                                      ),
+                                    ),
+                                    child: Icon(
+                                      Icons.add,
+                                      color: Colors.black,
+                                      size: 18,
+                                    ),
+                                  ), // Icon widget
+                                  Text('1'),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(width: 1),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(100),
+                                      ),
+                                    ),
+                                    child: Icon(
+                                      Icons.remove,
+                                      color: Colors.black,
+                                      size: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
                   ],
                 ),
               ),
             ),
+
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  top: BorderSide(
+                    width: 0.5,
+                    color: const Color.fromARGB(255, 182, 182, 182),
+                  ),
+                ),
+              ),
+
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    // small rounded corners
+                    // border
+                  ),
+                  backgroundColor: Colors.deepOrange,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                ),
+                child: Text(
+                  'Add to cart',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 5),
           ],
         ),
       ),
