dcfb2877939ef71a89b772a60bf8221383a780d7
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-11-26 22:58:18 +0100

    small page view onboarding

--- lib/onboarding/onBoarding.dart
+++ lib/onboarding/onBoarding.dart
@@ -1,7 +1,52 @@
 import 'package:flutter/material.dart';
 
-class Onboarding extends StatelessWidget {
+class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  final PageController _pageController = PageController();
+  List<int> pagination = [1, 2, 3, 4];
+
+  void nextPage() {
+    _pageController.nextPage(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeInExpo,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Text(
+            _pageController.page.toString(),
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 30),
+          PageView(
+            controller: _pageController,
+            children: [
+              OnboardingOne(nextScreen: nextPage),
+              OnboardingTwo(nextScreen: nextPage),
+              OnboardingThree(nextScreen: nextPage),
+              OnboardingFour(nextScreen: nextPage),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingOne extends StatelessWidget {
+  const OnboardingOne({super.key, required this.nextScreen});
+
+  final VoidCallback nextScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +58,8 @@ class Onboarding extends StatelessWidget {
           MaterialButton(
             minWidth: double.infinity,
             color: Colors.lightBlue,
+            onPressed: nextScreen,
             child: Text('Continue'),
-            onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => OnboardingTwo())),
           ),
         ],
       ),
@@ -23,7 +68,9 @@ class Onboarding extends StatelessWidget {
 }
 
 class OnboardingTwo extends StatelessWidget {
-  const OnboardingTwo({super.key});
+  const OnboardingTwo({super.key, required this.nextScreen});
+
+  final VoidCallback nextScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +82,56 @@ class OnboardingTwo extends StatelessWidget {
           MaterialButton(
             minWidth: double.infinity,
             color: Colors.lightBlue,
+            onPressed: nextScreen,
             child: Text('Continue'),
-            onPressed: () => print(''),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingThree extends StatelessWidget {
+  const OnboardingThree({super.key, required this.nextScreen});
+
+  final VoidCallback nextScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Text('Screen Three'),
+          Spacer(),
+          MaterialButton(
+            minWidth: double.infinity,
+            color: Colors.lightBlue,
+            onPressed: nextScreen,
+            child: Text('Continue'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingFour extends StatelessWidget {
+  const OnboardingFour({super.key, required this.nextScreen});
+
+  final VoidCallback nextScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Text('Screen Four'),
+          Spacer(),
+          MaterialButton(
+            minWidth: double.infinity,
+            color: Colors.lightBlue,
+            onPressed: nextScreen,
+            child: Text('Continue'),
           ),
         ],
       ),
