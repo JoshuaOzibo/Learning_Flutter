1156e4c338c51f4b2f6ebc5f0cddb035a55b6754
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-12-09 19:47:59 +0100

    form completed

--- lib/learning_auth/learning_auth_screen.dart
+++ lib/learning_auth/learning_auth_screen.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/learning_auth/widgets/email_field.dart';
 
-class LearningAuthScreen extends StatelessWidget {
+class LearningAuthScreen extends StatefulWidget {
   const LearningAuthScreen({super.key});
+
+  @override
+  State<LearningAuthScreen> createState() => _LearningAuthScreenState();
+}
+
+class _LearningAuthScreenState extends State<LearningAuthScreen> {
+  final TextEditingController emailController = TextEditingController();
+
+  void handleSubscribeButton() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 234, 234, 234),
-      body: Expanded(child: Center(
-        child: EmailField(),
-      )),
+      body: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              EmailField(
+                handleSubscribeButton: handleSubscribeButton,
+                emailController: emailController,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/learning_auth/widgets/email_field.dart
+++ lib/learning_auth/widgets/email_field.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
 class EmailField extends StatelessWidget {
-  const EmailField({super.key});
+  const EmailField({
+    super.key,
+    required this.handleSubscribeButton,
+    required this.emailController,
+  });
+
+  final VoidCallback handleSubscribeButton;
+  final TextEditingController emailController;
 
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    return Column(
+      spacing: 20,
+      children: [
+        TextField(
+          autocorrect: true,
+          keyboardType: TextInputType.emailAddress,
+          controller: emailController,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.blueGrey, style: BorderStyle.solid),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            contentPadding: EdgeInsets.all(20),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.green),
+            ),
+            hint: Text('Enter email'),
+            label: Text('Hello'),
+          ),
+        ),
+
+        MaterialButton(
+          color: Colors.brown,
+          height: 50,
+          minWidth: double.infinity,
+          onPressed: handleSubscribeButton,
+          child: Text('Subscribed', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
   }
 }
