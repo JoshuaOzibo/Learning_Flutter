739ba96b833d89ae4df5cbc117bfeb8132099afd
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-12-10 05:32:18 +0100

    state updated

--- lib/learning_auth/viewmodel_with_cubit/viewmodel_state.dart
+++ lib/learning_auth/viewmodel_with_cubit/viewmodel_state.dart
@@ -1,8 +1,27 @@
 class ViewmodelState {
-  String emailController;
-  bool isLoading;
-  String errorMessage;
-  bool isAuthenticated = false;
+  final String? emailController;
+  final bool? isLoading;
+  final String? errorMessage;
+  final bool isAuthenticated;
 
-  ViewmodelState(this.emailController, this.isLoading, this.errorMessage, {required this.isAuthenticated});
+  ViewmodelState({
+    this.emailController,
+    this.isLoading,
+    this.errorMessage,
+    required this.isAuthenticated,
+  });
+
+  ViewmodelState copyWith({
+    String? emailController,
+    bool? isLoading,
+    String? errorMessage,
+    bool? isAuthenticated,
+  }) {
+    return ViewmodelState(
+      emailController: emailController ?? this.emailController,
+      isLoading: isLoading ?? this.isLoading,
+      errorMessage: errorMessage ?? this.errorMessage,
+      isAuthenticated: isAuthenticated ?? this.isAuthenticated,
+    );
+  }
 }
